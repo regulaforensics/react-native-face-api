4651037d1045a07c24945e66cc8bbd8a8bc91249
Jenkins <jenkins@regula.by>
2022-06-21 13:50:14 +0300

    0.0.63

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,17 +1,16 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+const path = require('path');
+const folders = ["../", path.resolve(path.join(__dirname, './node_modules'))];
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
+  resolver: {
+    nodeModulesPaths: folders
+  },
+  watchFolders: folders,
 };
